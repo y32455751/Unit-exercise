e013f164f6aaf7de984ffac0efa97cd9a96620dc
KaenCat <390970925@qq.com>
2019-03-25 21:43:12 +0800

    这个项目是activemq的消息发送端。     持久化广播消息发送     自动确认消息发送     多方消费消息发送

--- activeMQ-Sender/src/main/java/com/activeMQ/example/JMSQueueSenderForNotTrue.java
+++ activeMQ-Sender/src/main/java/com/activeMQ/example/JMSQueueSenderForNotTrue.java
@@ -7,7 +7,7 @@ import javax.jms.*;
 /**
  * activeMQ 消息发送端
  */
-public class JMSQueueSender {
+public class JMSQueueSenderForNotTrue {
 
     public static void main(String[] args) {
         // 从连接工厂中获得连接，activemq使用tcp协议，默认传输端口为61616
@@ -20,17 +20,19 @@ public class JMSQueueSender {
             // 第一个参数决定session是否有事务性，类似于数据库事务性，有提交，回滚
             // session必须执行commit才能成功的消费，否则消息将会被重复消费
             // 当参数为false的时候，消息将会呗自动提交
-            Session session = connection.createSession(Boolean.TRUE,Session.AUTO_ACKNOWLEDGE);
+            Session session = connection.createSession(Boolean.FALSE,Session.AUTO_ACKNOWLEDGE);
             // 创建队列，参数为队列名称,表示监听这个队列，如果没有这个队列则会创建
             Destination destination = session.createQueue("TestQueue");
             // 创建信息发送者，针对某个队列进行发送，参数为队列
             MessageProducer producer = session.createProducer(destination);
-            // 创建被发送的信息，参数为信息，信息为多种格式，这里使用TextMessage作为举例
-            TextMessage textMessage = session.createTextMessage("Test Message");
-            // 发送信息
-            producer.send(textMessage);
+            for(int i=0;i<10;i++) {
+                // 创建被发送的信息，参数为信息，信息为多种格式，这里使用TextMessage作为举例
+                TextMessage textMessage = session.createTextMessage("Test Message" + i);
+                // 发送信息
+                producer.send(textMessage);
+            }
             // 提交上面的操作
-            session.commit();
+//            session.commit();
             session.close();
         } catch (JMSException e) {
             e.printStackTrace();
--- activeMQ-Sender/src/main/java/com/activeMQ/example/JMSTopicSender.java
+++ activeMQ-Sender/src/main/java/com/activeMQ/example/JMSTopicSender.java
@@ -7,7 +7,7 @@ import javax.jms.*;
 /**
  * activeMQ 消息发送端
  */
-public class JMSQueueSender {
+public class JMSTopicSender {
 
     public static void main(String[] args) {
         // 从连接工厂中获得连接，activemq使用tcp协议，默认传输端口为61616
@@ -22,11 +22,11 @@ public class JMSQueueSender {
             // 当参数为false的时候，消息将会呗自动提交
             Session session = connection.createSession(Boolean.TRUE,Session.AUTO_ACKNOWLEDGE);
             // 创建队列，参数为队列名称,表示监听这个队列，如果没有这个队列则会创建
-            Destination destination = session.createQueue("TestQueue");
+            Destination destination = session.createTopic("TestTopic");
             // 创建信息发送者，针对某个队列进行发送，参数为队列
             MessageProducer producer = session.createProducer(destination);
             // 创建被发送的信息，参数为信息，信息为多种格式，这里使用TextMessage作为举例
-            TextMessage textMessage = session.createTextMessage("Test Message");
+            TextMessage textMessage = session.createTextMessage("Test Message1");
             // 发送信息
             producer.send(textMessage);
             // 提交上面的操作
